6c722585d63e0d762b14a8e5c2956807a852da2c
cachitovalles <84902861+cachitovalles@users.noreply.github.com>
2021-08-16 13:59:03 +0200

    Update life.cc

--- life/life.cc
+++ life/life.cc
@@ -241,7 +241,7 @@ private:
 int main(int argc, char *argv[])
 {
 
-    int scroll_ms = 300;
+    int scroll_ms = rand () % 60 + 30;
 
     RGBMatrix::Options matrix_options;
     rgb_matrix::RuntimeOptions runtime_opt;

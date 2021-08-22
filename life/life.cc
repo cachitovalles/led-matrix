e94cb21fb892b50a19feb7c38c330b918c5ff1c4
cachitovalles <84902861+cachitovalles@users.noreply.github.com>
2021-08-22 20:38:58 +0200

    Update life.cc

--- life/life.cc
+++ life/life.cc
@@ -251,6 +251,12 @@ private:
                 {
                         newGameState_[x][y] = 1;
                 }
+                
+                 if (GameState_[x][y] == 1 && (num > 3) && (count == 20)) //Regla 1
+                {
+                        newGameState_[x][y] = 0;
+                }
+                
                 if (count == 20)  // Nacen cada 10 Iteracciones
                 {
                     count = 0;

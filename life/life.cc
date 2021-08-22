3be1fe16ed5a6a9d58fedc9f1d0ddfadabc03465
cachitovalles <84902861+cachitovalles@users.noreply.github.com>
2021-08-22 13:31:38 +0200

    Update life.cc

--- life/life.cc
+++ life/life.cc
@@ -131,7 +131,7 @@ public:
                     if (GameState_[x][y] < 0.5)
                     {
                        float f = min(1, max(0, newGameState_[x][y]));
-                       canvas()->SetPixel(x, y, r_ * f, b_ * f, g_ * f); // esto era r_, g_, b_ VIVAS
+                       canvas()->SetPixel(x, y, 250, 0, 0); // esto era r_, g_, b_ VIVAS
                     }
                     else
                        canvas()->SetPixel(x, y, r_, b_, g_); //esto era 0, 0, 0 MUERTAS

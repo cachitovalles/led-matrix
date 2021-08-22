8b3c9356c33951028923c063e311694857e4cd32
cachitovalles <84902861+cachitovalles@users.noreply.github.com>
2021-08-22 11:53:10 +0200

    Update life.cc

--- life/life.cc
+++ life/life.cc
@@ -130,7 +130,7 @@ public:
                    
                     if (GameState_[x][y])
                        canvas()->SetPixel(x, y, r_, b_, g_); // esto era r_, g_, b_ VIVAS
-                    else
+                    elif
                        canvas()->SetPixel(x, y, 0, 0, 0); //esto era 0, 0, 0 MUERTAS
                        
                     if (GameState_[x][y])

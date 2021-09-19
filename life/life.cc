2a56a4ec0f9866a2fa8b8eddc17323710982a705
cachitovalles <84902861+cachitovalles@users.noreply.github.com>
2021-09-19 21:28:37 +0200

    Update life.cc

--- life/life.cc
+++ life/life.cc
@@ -254,11 +254,11 @@ private:
                 if (count == 20)  // Nacen cada 10 Iteracciones
                 {
                     count = 0;
-                    newGameState_[x / 2 + 11][y / 2 + 11] = 1;
-                    newGameState_[x / 2 + 12][y / 2 + 12] = 1;
-                    newGameState_[x / 2 + 12][y / 2 + 13] = 1;
-                    newGameState_[x / 2 + 11][y / 2 + 13] = 1;
-                    newGameState_[x / 2 + 10][y / 2 + 13] = 1;
+                    newGameState_[x / 2 + 21][y / 2 + 21] = 1;
+                    newGameState_[x / 2 + 22][y / 2 + 22] = 1;
+                    newGameState_[x / 2 + 22][y / 2 + 23] = 1;
+                    newGameState_[x / 2 + 21][y / 2 + 23] = 1;
+                    newGameState_[x / 2 + 20][y / 2 + 23] = 1;
                 }
                  
                 if (GameState_[x][y] > 0.5 && (num > 2 || num < 3)) //Regla 1
